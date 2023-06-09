7615b794f0e989b77aa675bf72432bc3369f1fed
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2023-06-09 12:31:55 -0300

    corrigindo erro

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
-import express from 'express';
+// import express from 'express';
+import express, { Router } from 'express';
 
-const app = express();
+const app = express()
+const route = Router();
 
-app.get('/', (request, response) => {
+route.get('/api/ola/:info', () => { console.log('ola') });
+
+route.get('/', (request, response) => {
     return response.json({ message: 'Hello World' });
 });
+
+app.use(express.json());
+app.use(route);
 
 app.listen(3333, () => {
     console.log('Server started on port 3333!');
